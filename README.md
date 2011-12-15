a0045446c989d69b7ae61125b4abccc7afc7899c
Tom Scott <tubbo@psychedeli.ca>
2011-12-15 14:16:32 -0500

    created the jQuery.defacto plugin and a README
    the README explains more about the plugin, while jquery.defacto.js is
    the entire contents of the plugin.

---
+++ README.md
@@ -0,0 +1,16 @@
+Defacto: A default text plugin for humans.
+==========================================
+
+jQuery.defacto is a [jQuery][1] plugin that replaces the `value` of a given `<input>` field with the text you type when focused. It allows for default text to be embedded in text/password inputs, and ensures consistent functionality across all browsers. It was designed to be simple, straightforward, and most of all easy to use. 
+
+Features
+--------
+
+- Cross-browser compatible, tested in Firefox 3.6+, all versions of Chrome/Safari, and Internet Explorer 5.5+
+- Changeable default text color
+
+Roadmap
+-------
+
+- Dim default text until typing occurs (a la apple.com)
+- Leverage existing HTML5 default text features in modern browsers
---
+++ jquery.defacto.js
@@ -0,0 +1,93 @@
+/**
+ * jQuery.defacto is a plugin for default text. It is an extraction of some functions written at A + L DESIGN 
+ * for the purpose of providing default text to input fields that needed it. The plugin has a built-in password
+ * type changer that is cross-browser (and jQuery, of course) compliant, as well as provisions to change the color
+ * of the default text.
+ *
+ * @author Tom Scott <tom@aplusldesign.com> & Mike Dombrowski <mike@aplusldesign.com>
+ */ 
+$.fn.defacto = function() {
+	/**
+	 * Container for functions
+	 *
+	 * @param {jQuery}  o - DOM element
+	 * @param {Options} c - config settings
+	 * @constructor
+	 */
+	var DefaultInput = function(o, c) {
+		var self = this;
+		
+		/**
+		 * Binds all events and constructs the plugin.
+		 *
+		 * @constructor
+		 */
+		this.bind = function() {
+			var v = o.attr('value');
+			
+			o.data('default', v)
+			o.bind('focus',function() { self.clear(); })
+			o.bind('blur',function() { self.replace(); });
+			
+			if (c.defaultColor) {
+				o.css({color: c.defaultColor});
+			}
+		};
+		
+		/**
+		 * Toggles typed text and default text.
+		 */
+		this.replace = function() {
+			var v = o.attr('value');
+			var d = o.data('default');
+			var p = o.hasClass('password');
+			
+			if (v=='') o.attr('value',d);
+			if (p && v=='') {
+				if (c.defaultColor) {
+					var tClone = o.clone().attr('type','text').css({color: c.defaultColor}); 
+				} else {
+					var tClone = o.clone().attr('type','text');
+				}
+				var tClone =  o.clone().attr('type','text');
+				o.replaceWith(tClone);
+				tClone.bind('focus',function() { clearText($(this)); }).data('default',d).attr('value',d).blur();
+			}
+		};
+		
+		/**
+		 * Clears the input's default text to prepare for typing.
+		 */
+		this.clear = function() {
+			var v = o.attr('value');
+			var d = o.data('default');
+			var p = (o.hasClass('password') || (o.attr('type') == 'password'));
+			
+			if (v==d) o.attr('value','');
+			if (p) {
+				var pClone = o.clone().attr('type','password').css({color: o.data('color')});
+				o.replaceWith(pClone)
+				pClone.bind('blur',function() { replaceText($(this)); }).data('default',d).focus();
+			}
+		};
+		
+		this.bind();
+	};
+	
+	/**
+	 * Runtime
+	 */
+	return this.each(function(options) {
+		var element = $(this);
+		
+		var config = {
+			defaultColor: '',
+			dimColor: '',
+			dimmer: false
+		};
+		
+		$.fn.extend(config, options);
+		
+		var di = new DefaultInput(element, config);
+	});
+};
